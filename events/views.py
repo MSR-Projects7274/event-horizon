7982d2bf3ab5d1892959bc2e1f3c8726fb450900
MSR-Projects7274 <m.roberts@cosmic.me.uk>
2026-08-17 14:21:38 +0100

    Add events details to Django, add CSS and cards to events page

--- events/views.py
+++ events/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Event
+
+
+def event_list(request):
+    """Display all active upcoming events."""
+    events = Event.objects.filter(active=True)
+
+    return render(
+        request,
+        'events/event_list.html',
+        {'events': events}
+    )
+
+
+def event_detail(request, event_id):
+    """Display the details of a single event."""
+    event = get_object_or_404(
+        Event,
+        id=event_id,
+        active=True
+    )
+
+    return render(
+        request,
+        'events/event_detail.html',
+        {'event': event}
+    )
