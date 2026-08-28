5610ed38143ce76852ac3bf824f41f1604d964f7
MSR-Projects7274 <m.roberts@cosmic.me.uk>
2026-08-28 13:00:41 +0100

    Improve production security settings

--- event_horizon/settings.py
+++ event_horizon/settings.py
@@ -43,6 +43,11 @@ ALLOWED_HOSTS = os.environ.get(
     'localhost,127.0.0.1'
 ).split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    'CSRF_TRUSTED_ORIGINS',
+    'http://localhost:8000'
+).split(',')
+
 
 # Application definition
 
@@ -170,7 +175,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STRIPE_PUBLIC_KEY = env('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
-STRIPE_WH_SECRET = env('STRIPE_WH_SECRET', default='')
+STRIPE_WH_SECRET = env('STRIPE_WH_SECRET')
 
 # Resend Environment Variables
 
